7423aa3c00d6fc66374cd3b66954b538e2a00e9d
tranduchung308 <tranduchung308@gmail.com>
2023-07-04 01:23:07 +0700

    git commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import {
   useNavigate,
 } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Work from "./Pages/Work/Work";
 
 function App() {
   return (
     <div className="App">
       <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/friend" element={<Work />} />
       </Routes>
     </div>
   );
